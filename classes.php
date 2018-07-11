e7cb7b988b2427ba54034a5d3100b0475f691e7a
PerhapsSomeone <ma2003br@gmail.com>
2018-07-11 20:18:11 +0200

    Added regex to verify that UUIDs have the correct format.

--- classes.php
+++ classes.php
@@ -91,7 +91,7 @@ class analytics
         $page = $_GET["page"];
 
         if (isset($_COOKIE["uuid"])) { // Check whether client already has UUID Cookie
-            if (strlen($_COOKIE["uuid"]) === 36) { // If UUID Length bigger/smaller than 32 it's invalid.
+            if (strlen($_COOKIE["uuid"]) === 36 && preg_match("/[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}/", $_COOKIE["uuid"]) === 1) { // If UUID Length is bigger/smaller than 32 or doesnt match the Regex, it's invalid.
                 $uuid = $_COOKIE["uuid"];
             } else {
                 $uuid = self::generateUUID(); // Regenerate UUID and set Cookie.
